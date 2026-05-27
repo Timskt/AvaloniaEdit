8d65fa39c2d141348cc957e8799f912a2f8e0167
Timskt <1149177967@qq.com>
2026-05-28 01:01:08 +0800

    feat: render cursor position within IME preedit text
    When the IME provides a cursorOffset (Avalonia 12 SetPreeditText(string,
    int?) overload), draw a visible cursor at the specified position within
    the composition text. This gives users visual feedback on where the next
    character will be inserted during IME composition.

--- src/AvaloniaEdit/Rendering/PreeditLayer.cs
+++ src/AvaloniaEdit/Rendering/PreeditLayer.cs
@@ -85,6 +85,23 @@ namespace AvaloniaEdit.Rendering
             drawingContext.DrawLine(pen,
                 new Point(origin.X, origin.Y + height - 1),
                 new Point(origin.X + width, origin.Y + height - 1));
+
+            // Draw cursor within preedit text when cursorOffset is specified
+            if (_cursorOffset.HasValue && _cursorOffset.Value >= 0 && _cursorOffset.Value <= _preeditText.Length)
+            {
+                var prefixText = _preeditText.Substring(0, _cursorOffset.Value);
+                var prefixLayout = new TextLayout(
+                    prefixText,
+                    typeface,
+                    fontRenderingEmSize,
+                    foreground,
+                    textWrapping: TextWrapping.NoWrap);
+                var cursorX = origin.X + prefixLayout.WidthIncludingTrailingWhitespace;
+                var cursorPen = new ImmutablePen(foreground.ToImmutable(), 2);
+                drawingContext.DrawLine(cursorPen,
+                    new Point(cursorX, origin.Y),
+                    new Point(cursorX, origin.Y + height));
+            }
         }
     }
 }
