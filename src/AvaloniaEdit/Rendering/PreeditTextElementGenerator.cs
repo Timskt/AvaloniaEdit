e4d69da108c30cf679c6bf5d7f1657474a64ef43
Timskt <1149177967@qq.com>
2026-06-07 23:39:50 +0800

    Stabilize inline IME rendering docs

--- src/AvaloniaEdit/Rendering/PreeditTextElementGenerator.cs
+++ src/AvaloniaEdit/Rendering/PreeditTextElementGenerator.cs
@@ -1,9 +1,7 @@
 using System;
-using Avalonia;
 using Avalonia.Media;
 using Avalonia.Media.TextFormatting;
 using AvaloniaEdit.Editing;
-using AvaloniaEdit.Utils;
 
 namespace AvaloniaEdit.Rendering
 {
@@ -77,61 +75,16 @@ namespace AvaloniaEdit.Rendering
 
         public override TextRun CreateTextRun(int startVisualColumn, ITextRunConstructionContext context)
         {
+            TextRunProperties.SetTextDecorations(TextDecorations.Underline);
             var relativeOffset = Math.Max(0, startVisualColumn - VisualColumn);
             relativeOffset = Math.Min(relativeOffset, Text.Length);
-            return new PreeditTextRun(this, relativeOffset, context.TextView, TextRunProperties);
+            return new TextCharacters(Text.AsMemory(relativeOffset), TextRunProperties);
         }
 
         public override ReadOnlyMemory<char> GetPrecedingText(int visualColumnLimit, ITextRunConstructionContext context)
         {
             var length = Math.Max(0, Math.Min(visualColumnLimit - VisualColumn, Text.Length));
             return Text.AsMemory(0, length);
-        }
-    }
-
-    internal sealed class PreeditTextRun : DrawableTextRun
-    {
-        private readonly TextLine _textLine;
-        private readonly TextLine _prefixLine;
-        private readonly int _relativeOffset;
-
-        public PreeditTextRun(PreeditTextElement element, int relativeOffset, TextView textView, TextRunProperties properties)
-        {
-            Element = element ?? throw new ArgumentNullException(nameof(element));
-            Properties = properties ?? throw new ArgumentNullException(nameof(properties));
-            _relativeOffset = Math.Max(0, Math.Min(relativeOffset, element.Text.Length));
-            Text = element.Text.AsMemory(_relativeOffset);
-
-            var formatter = TextFormatterFactory.Create(textView);
-            _textLine = FormattedTextElement.PrepareText(formatter, Text.ToString(), properties);
-            var cursorOffset = Math.Max(_relativeOffset, element.CursorOffset) - _relativeOffset;
-            cursorOffset = Math.Max(0, Math.Min(cursorOffset, Text.Length));
-            _prefixLine = FormattedTextElement.PrepareText(formatter, Text.Slice(0, cursorOffset).ToString(), properties);
-        }
-
-        public PreeditTextElement Element { get; }
-
-        public override ReadOnlyMemory<char> Text { get; }
-
-        public override TextRunProperties Properties { get; }
-
-        public override double Baseline => _textLine.Baseline;
-
-        public override Size Size => new Size(_textLine.WidthIncludingTrailingWhitespace, _textLine.Height);
-
-        public override void Draw(DrawingContext drawingContext, Point origin)
-        {
-            _textLine.Draw(drawingContext, origin);
-            var foreground = Properties.ForegroundBrush ?? Brushes.Black;
-            var underlineY = origin.Y + _textLine.Height - 1;
-            drawingContext.DrawLine(new Pen(foreground, 1),
-                new Point(origin.X, underlineY),
-                new Point(origin.X + _textLine.WidthIncludingTrailingWhitespace, underlineY));
-
-            var cursorX = origin.X + _prefixLine.WidthIncludingTrailingWhitespace;
-            drawingContext.DrawLine(new Pen(foreground, 1),
-                new Point(cursorX, origin.Y),
-                new Point(cursorX, origin.Y + _textLine.Height));
         }
     }
 }
